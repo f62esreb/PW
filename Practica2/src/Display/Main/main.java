60b4a18c9acd9027acb2491d421abae7a6199747
Pabloogii <91120243+Pabloogii@users.noreply.github.com>
2024-12-12 16:51:16 +0100

    Update main.java

--- Practica2/src/Display/Main/main.java
+++ Practica2/src/Display/Main/main.java
@@ -1,1 +1,23 @@
+package display.main;
+
+import business.GestorUsuarios;
+import business.jugadores.JugadorDTO;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        GestorUsuarios gestor = new GestorUsuarios();
+
+        try {
+            JugadorDTO nuevoJugador = new JugadorDTO(0, "Juan Pérez", new Date(90, 5, 24), new Date(), "juan.perez@example.com");
+            gestor.registrarJugador(nuevoJugador);
+
+            System.out.println("Jugadores registrados:");
+            gestor.listarUsuarios().forEach(System.out::println);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
 
