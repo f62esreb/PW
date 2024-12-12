6a47266cd87ce618bf6620879a68376ea9403676
Pabloogii <91120243+Pabloogii@users.noreply.github.com>
2024-12-12 16:48:47 +0100

    Update gestorUsuarios.java

--- Practica2/src/bussiness/Gestores/gestorUsuarios.java
+++ Practica2/src/bussiness/Gestores/gestorUsuarios.java
@@ -1,1 +1,23 @@
+package business;
+
+import business.jugadores.JugadorDTO;
+import data.dao.JugadorDAO;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class GestorUsuarios {
+    private final JugadorDAO jugadorDAO = new JugadorDAO();
+
+    public void registrarJugador(JugadorDTO jugador) throws SQLException {
+        if (jugadorDAO.obtenerJugadorPorId(jugador.getId()) != null) {
+            throw new IllegalArgumentException("El jugador ya está registrado.");
+        }
+        jugadorDAO.insertarJugador(jugador);
+    }
+
+    public List<JugadorDTO> listarUsuarios() throws SQLException {
+        return jugadorDAO.listarJugadores();
+    }
+}
 
